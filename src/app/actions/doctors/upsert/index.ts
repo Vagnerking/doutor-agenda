8fa15fbd6d69cce4c556729162f1269ba3909093
Vagner Araújo <gnerprocs@gmail.com>
2025-05-29 23:41:38 -0300

    feat: add dayjs dependency and implement UTC conversion for available time in doctor upsert action

--- src/app/actions/doctors/upsert/index.ts
+++ src/app/actions/doctors/upsert/index.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 import { headers } from "next/headers";
 
 import { db } from "@/db";
@@ -10,9 +12,26 @@ import { convertClinicDatFromServerSession } from "@/utils/clinic/clinic-helper"
 
 import { upsertDoctorSchema } from "./schema";
 
+dayjs.extend(utc);
+
 export const upsertDoctor = actionClient
   .schema(upsertDoctorSchema)
   .action(async ({ parsedInput }) => {
+    const availableFromTime = parsedInput.availableFromTime;
+    const availableToTime = parsedInput.availableToTime;
+
+    const availableFromTimeUTC = dayjs()
+      .set("hour", parseInt(availableFromTime.split(":")[0]))
+      .set("minute", parseInt(availableFromTime.split(":")[1]))
+      .set("second", parseInt(availableFromTime.split(":")[2]))
+      .utc();
+
+    const availableToTimeUTC = dayjs()
+      .set("hour", parseInt(availableToTime.split(":")[0]))
+      .set("minute", parseInt(availableToTime.split(":")[1]))
+      .set("second", parseInt(availableToTime.split(":")[2]))
+      .utc();
+
     const result = await auth.api.getSession({
       headers: await headers(),
     });
@@ -35,11 +54,15 @@ export const upsertDoctor = actionClient
         id: parsedInput.id,
         clinicId: clinic?.id ?? "",
         ...parsedInput,
+        availableFromTime: availableFromTimeUTC.format("HH:mm:ss"),
+        availableToTime: availableToTimeUTC.format("HH:mm:ss"),
       })
       .onConflictDoUpdate({
         target: [doctorsTable.id],
         set: {
           ...parsedInput,
+          availableFromTime: availableFromTimeUTC.format("HH:mm:ss"),
+          availableToTime: availableToTimeUTC.format("HH:mm:ss"),
         },
       });
   });
